f3db01fdc984b8a62f85892598b19ce8956dd42c
danmtnezcas <danmtnezcas@outlook.com>
2022-05-10 01:12:57 -0500

    Agregando archivo Reader.js para la lectura del archivo

---
+++ app/lib/utils/Reader.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+class Reader{
+    static readJsonFile(path) {
+        const rawdata = fs.readFileSync(path);
+        const data = JSON.parse(rawdata);
+        return data;
+    }
+}
+
+module.exports = Reader;
