27684d770c427a7664bdeed9b4bb0372fd0986d4
danmtnezcas <danmtnezcas@outlook.com>
2022-05-10 00:47:12 -0500

    Añadiendo archivo Reader.test.js con prueba para Reader.js

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,13 @@
+const Reader = require("../../../app/lib/utils/Reader");
+
+describe('Unit Test for Reader class', () => {
+    test("Read json file, we should have names from Woopa1 to Woopa15", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        expect(explorers).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({"name": "Woopa1"}),
+                expect.objectContaining({"name": "Woopa15"})
+            ])
+        )
+    })
+})
