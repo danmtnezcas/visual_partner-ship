f2fb9cdee0d657528143bbc9b614ccedf36999dc
danmtnezcas <danmtnezcas@outlook.com>
2022-05-10 02:44:37 -0500

    Modificando archivo ExplorerService.js, agregando código para el método getAmountOfExplorersByMission

--- app/lib/services/ExplorerService.js
+++ app/lib/services/ExplorerService.js
@@ -3,6 +3,11 @@ class ExplorerService{
         const explorersInNode = explorers.filter(explorer => explorer.mission == mission);
         return explorersInNode;
     }
+    static getAmountOfExplorersByMission(explorers, mission) {
+        const explorersInNode = this.filterByMission(explorers, mission);
+        const numberOfExplorersInNode = explorersInNode.length;
+        return numberOfExplorersInNode;
+    }
 }
 
 module.exports = ExplorerService;
