1b8bce3ab851ba44cee7c36560923eead2d74634
danmtnezcas <danmtnezcas@outlook.com>
2022-05-10 02:40:19 -0500

    Añadiendo archivo ExplorerService.test.js con prueba para el método filterByMission

---
+++ test/lib/services/ExplorerService.test.js
@@ -0,0 +1,16 @@
+const Reader = require("../../../app/lib/utils/Reader");
+const ExplorerService = require('../../../app/lib/services/ExplorerService');
+
+describe('Unit Tests for ExplorerService class', () => {
+    test("Get the explorers in node, we should only have explorers in node", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        expect(explorersInNode).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({"name": "Woopa1"}), // This explorer is in node
+                //expect.objectContaining({"name": "Woopa9"}), // This explorer isn't in node
+                expect.objectContaining({"name": "Woopa15"}) // This explorer is in node
+            ])
+        )
+    })
+})
