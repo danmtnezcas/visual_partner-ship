dbc84a6ad74e7e5adcfd459ab418e6f7906a1f75
danmtnezcas <danmtnezcas@outlook.com>
2022-05-14 06:13:08 -0500

    Añadiendo archivo FizzbuzzService.js con código para el método applyValidationInExplorer

---
+++ app/lib/services/FizzbuzzService.js
@@ -0,0 +1,23 @@
+class FizzbuzzService {
+    static  applyValidationInExplorer(explorers) {
+        const explorersInNodeApplyValidation = [];
+        explorers.forEach((explorer) => {
+            if(explorer.score%5 === 0 && explorer.score%3 === 0){
+                explorer.trick = "FIZZBUZZ";
+                explorersInNodeApplyValidation.push(explorer);
+            } else if(explorer.score%5 === 0) {
+                explorer.trick = "BUZZ";
+                explorersInNodeApplyValidation.push(explorer);
+            } else if(explorer.score%3 === 0) {
+                explorer.trick = "FIZZ";
+                explorersInNodeApplyValidation.push(explorer);
+            } else {
+                explorer.trick = explorer.score;
+                explorersInNodeApplyValidation.push(explorer);
+            }
+        })
+        return explorersInNodeApplyValidation;
+    }
+}
+
+module.exports = FizzbuzzService;
